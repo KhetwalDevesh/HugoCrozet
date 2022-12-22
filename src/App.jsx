76f7fcfb858ac75262bc3c868a23291038d689f0
khetwaldevesh <deveshkhetwal05@gmail.com>
2022-12-22 19:59:56 +0530

    motionOne animation

--- src/App.jsx
+++ src/App.jsx
@@ -40,30 +40,41 @@ function App() {
     inView(projectsClass, () => {
       const oddNoProject = document.querySelectorAll('.odd-no-project');
       const evenNoProject = document.querySelectorAll('.even-no-project');
-      animate(oddNoProject, { transform: 'none' }, { duration: 4 });
-      animate(evenNoProject, { transform: 'none' }, { duration: 4 });
+      animate(
+        oddNoProject,
+        // { root: projectsClass },
+        { transform: 'none' },
+        { duration: 4 }
+      );
+      animate(
+        evenNoProject,
+        // { root: projectsClass },
+        { transform: 'none' },
+        { duration: 4 }
+      );
     });
     // });
   }, []);
   return (
-    <div className=" h-full w-screen flex flex-col bg-clay">
-      <div className=" fixed border-2 m-0 backdrop-blur border-yellow-500 p-7 flex justify-between uppercase font-lato text-xl w-full">
-        <div className="header-text-class">Hugo Crozet</div>
+    <div className=" overflow-hidden flex flex-col bg-clay">
+      <div className=" fixed  m-0  backdrop-blur  p-7 flex justify-between uppercase font-lato text-xl w-full">
+        <div className="header-text-class">Khetwal Devesh</div>
         <div className="header-text-class">Contact</div>
       </div>
       <div className="h-24"></div>
-      <div className="border-2 border-blue-500 h-full flex flex-col items-center justify-center font-maklight uppercase text-[220px]">
-        <div className="ml-0 border-2 border-blue-700 creative flex justify-start w-full">
+      <div className="  h-full flex flex-col items-center justify-center font-maklight uppercase text-[220px]">
+        <div className="ml-0  border-blue-700 creative flex justify-start w-full">
           Creative
         </div>
-        <div className="mr-0  border-2 border-blue-700 developer flex justify-end w-full">
-          {/* <span className="border-2 border-red-700"> </span> */}Developer
+        <div className="mr-0   border-blue-700 developer flex justify-end w-full">
+          {/* <span className=" border-red-700"> </span> */}Developer
         </div>
       </div>
-      <div className="w-1/4 ml-auto  p-5 border-2 border-red-700">
-        I AM HUGO, CREATIVE DEVELOPER BASED IN FRANCE. I WORK AS A FREELANCER
-        WITH AGENCIES, START-UPS AND INDIVIDUALS. I HAVE A FONDNESS FOR CLEAN
-        DESIGNS, BEAUTIFUL TYPOGRAPHIES AND INTERACTIVE DEVELOPMENT.
+      <div className="w-1/4 ml-auto  p-5  border-red-700">
+        I AM DEVESH, CREATIVE DEVELOPER BASED IN INDIA. I HAVE A FONDNESS FOR
+        CLEAN DESIGNS, BEAUTIFUL TYPOGRAPHIES AND INTERACTIVE DEVELOPMENT.I LIKE
+        TO CREATE LANDING PAGES USING CODE. I LIKE TO CREATE FRONTEND USING
+        CODE.
       </div>
       <div className="font-maklight uppercase text-[220px] p-16 mb-36">
         Projects
